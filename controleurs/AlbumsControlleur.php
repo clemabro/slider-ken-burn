da9f92f55af9584b7277b417242c9218a3be8b7d
EtoileNocturne <tarik_yildiz@hotmail.fr>
2020-04-22 17:46:22 +0200

    blabla

--- controleurs/AlbumsControlleur.php
+++ controleurs/AlbumsControlleur.php
@@ -1,6 +1,5 @@
 <?php
 
-require  __DIR__.'/../modeles/Managers.php';
 
 function albums()
 {
@@ -9,18 +8,22 @@ function albums()
     $cheminsImage = array();
     $donneesREL = getRelUserImageSliderManager()->getRelUserImageSliderByLogin('EtoileNocturne');
     var_dump($donneesREL);
-    foreach ($donneesREL as $relationI)
-    {
+
+    if (!empty($donneesREL)){
         echo 'HELLOOOO';
-        var_dump($relationI);
-        if (!array_key_exists($relationI->getIdSlider(),$identifiantsEnvoyes))
+        foreach ($donneesREL as $relationI)
         {
-            array_push($donneesSlider,getSliderManager()->getSliderById($relationI->getIdSlider()));
-            //$cheminsImage[strval($relationI->getIdSlider())] = getImageManager()->getImageById($relationI->getIdImage());
-            array_push($identifiantsEnvoyes,$relationI->getIdSlider());
+            echo 'HELLOOOO';
+            var_dump($relationI);
+            if (!array_key_exists($relationI->getIdSlider(),$identifiantsEnvoyes))
+            {
+                array_push($donneesSlider,getSliderManager()->getSliderById($relationI->getIdSlider()));
+                //$cheminsImage[strval($relationI->getIdSlider())] = getImageManager()->getImageById($relationI->getIdImage());
+                array_push($identifiantsEnvoyes,$relationI->getIdSlider());
+
+            }
 
         }
-
     }
 
     require __DIR__.'/../vues/Albums/albums.php';
