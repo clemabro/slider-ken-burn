8cf3170468f0ee4daf4ee94dcfbeef67a10e240b
Clément AZIBEIRO <clement.azibeiro@bts-malraux.net>
2020-04-23 13:42:55 +0200

    404

--- vues/404.php
+++ vues/404.php
@@ -1,3 +1,5 @@
 <?php
+    require_once 'vues/ressources/header.php';
 ?>
-<h1>404</h1>
+<h1 class="text-center">404 oh y a des mange morts ici je repars</h1>
+<img src="https://s1.dmcdn.net/v/9thCi1LN1qsahTqCP/x1080">
