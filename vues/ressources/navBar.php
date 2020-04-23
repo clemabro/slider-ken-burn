7da46a8664e1c08e70ad1648408f3cdd92c01286
Clément AZIBEIRO <clement.azibeiro@bts-malraux.net>
2020-04-23 11:37:53 +0200

    Navbar

--- vues/ressources/navBar.php
+++ vues/ressources/navBar.php
@@ -1,16 +1,13 @@
 <header id="header">
     <div class="navbar navbar-light box-shadow">
         <div class="container d-flex justify-content-between">
-            <a class="navbar-brand" href="#" id="header-logo" style="width:50px!important;">
+            <a class="navbar-brand" href="albums" id="header-logo" style="width:50px!important;">
                 <img src="vues/img/logo.png" class="img-fluid" alt="Logo">
             </a>
             
         </div>
-        <div class="my-2 my-lg-0">
-            <a href="#" class="btn btn-secondary my-2 my-lg-0">Déconnexion</a>
-            <form id="deco" action="deconnexion" method="POST">
-                <a href="#" onclick="$('#deco').submit()" class="btn btn-secondary my-2 my-lg-0">Déconnexion</a>
-            </form>
-        </div>
+        
+        <a href="albums" class="btn btn-primary mr-sm-2">Ma Collection</a>
+        <a href="deconnexion" class="btn btn-secondary my-2 my-sm-0">Déconnexion</a>
     </div>
 </header>
